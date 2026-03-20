99e8d918a2c4bf5612a377c71bc5b880cbeaa77d
nnathalia <lopesnnathallia.com>
2026-03-20 08:53:14 -0400

    create feature section

---
+++ src/components/feature-section/feature-section.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export function FeatureSection() {
+    return (
+        <section className="container grid gap-6 md:grid-cols-2 pb-8 pt-8 md:py-10">
+            <div className="flex flex-col gap-4 rounded-lg p-6 bg-gray-500 md:p-12 ">
+                <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">SIMPLES</span>
+                <h2 className="text-gray-100 text-heading-lg">Crie um catálogo de produtos online em poucos minutos</h2>
+            </div>
+            <div className="flex flex-col gap-4 rounded-lg p-6 bg-gray-500 md:p-12 ">
+                <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">PRÁTICO</span>
+                <h2 className="text-gray-100 text-heading-lg">Venda para seu público através de uma plataforma única</h2>
+            </div>
+            <div className="col-span-full flex flex-col gap-2">
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-4 rounded-lg bg-gray-500 p-6 md:p-12 ">
+                    <div className="flex flex-col gap-4">
+                        <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">PERSONÁLIZAVEL</span>
+                        <h2 className="text-gray-100 text-heading-lg">Tenha uma loja online personalizada com a cara da sua marca</h2>
+                        <Button className="rounded-full hidden md:flex mt-4 md:mt-auto w-fit">
+                            <Link href="/criar-loja" className="flex items-center gap-2">
+                                Criar loja grátis
+                                <ArrowRight />
+                            </Link>
+                        </Button>
+                    </div>
+
+
+                    <div className="flex flex-col items-center justify-center w-full">
+                        <div className="w-full max-w-md overflow-hidden">
+                            <Image
+                                src="/feature-section.svg"
+                                alt="Features"
+                                width={440}
+                                height={330}
+                                className="object-cover w-full"
+                            />
+                        </div>
+
+                        <Button className="w-full rounded-full  md:hidden mt-4 md:mt-auto min-w-fit">
+                            <Link href="/criar-loja" className="flex items-center gap-2">
+                                Criar loja grátis
+                                <ArrowRight />
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
